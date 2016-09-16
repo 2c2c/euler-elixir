9d908c77c810fe890c00725bd84f166a895ca6bf
Craig Collier <tehoreoz@gmail.com>
2016-09-15 20:41:03 -0700

    p7

--- lib/euler.ex
+++ lib/euler.ex
@@ -70,11 +70,43 @@ defmodule Euler do
   What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
   """
   def p5 do
-    Stream.iterate(21, fn(n) -> n + 1 end)
+    Stream.iterate(20, fn(n) -> n + 20 end)
     |> Enum.find(fn(n) -> divisible?(n) end)
   end
 
   defp divisible?(n) do
     Enum.all?(11..20, fn(d) -> rem(n,d) == 0 end)
+  end
+
+  @doc """
+  The sum of the squares of the first ten natural numbers is,
+
+  1^2 + 2^2 + ... + 10^2 = 385
+  The square of the sum of the first ten natural numbers is,
+
+  (1 + 2 + ... + 10)2 = 552 = 3025
+  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+  """
+  def p6 do
+    hundred = 1..100
+
+    squares = Enum.map(hundred, fn(x) -> x*x end)
+
+    difference =  (:math.pow(Enum.sum(hundred), 2)) - (Enum.sum(squares))
+  end
+
+  @doc """
+  By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+  What is the 10 001st prime number?
+  """
+  def p7 do
+    n = 10_001
+    Stream.iterate(0, &(&1) + 1)
+    |> Stream.filter(&prime?/1)
+    |> Enum.take(n)
+    |> List.last
   end
 end
