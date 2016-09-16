5b2cec6a90ed9edbf4abf062dd1789599b7958c7
Craig Collier <tehoreoz@gmail.com>
2016-09-15 20:12:54 -0700

    p3-5

--- lib/euler.ex
+++ lib/euler.ex
@@ -36,6 +36,45 @@ defmodule Euler do
 
   What is the largest prime factor of the number 600851475143 ?
   """
-  def p2 do
+  def p3 do
+    n = 600851475143
+    Stream.iterate(0, fn(x) -> x + 1 end)
+      |> Stream.filter(fn(x) -> prime?(x) end)
+      |> Stream.take_while(fn(x) -> x < round(:math.sqrt(n)) end)
+      |> Enum.filter(fn(x) -> rem(n, x) == 0 end)
+      |> List.last
+  end
+
+  def prime?(x) when x in [2, 3, 5, 7, 11, 13, 17], do: true
+  def prime?(x) do
+    Enum.all?((2..round(:math.sqrt(x))), fn(n) -> rem(x, n) != 0 end)
+  end
+
+  @doc """
+  A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+  Find the largest palindrome made from the product of two 3-digit numbers.
+  """
+  def p4 do
+    numbers = for i <- 999..100,
+                  j <- 999..100, do: {i,j}
+
+    Stream.map(numbers, fn({i,j}) -> i*j end)
+    |> Stream.filter(fn(x) -> to_charlist(x) == to_charlist(x) |> Enum.reverse end)
+    |> Enum.max
+  end
+
+  @doc """
+  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  """
+  def p5 do
+    Stream.iterate(21, fn(n) -> n + 1 end)
+    |> Enum.find(fn(n) -> divisible?(n) end)
+  end
+
+  defp divisible?(n) do
+    Enum.all?(11..20, fn(d) -> rem(n,d) == 0 end)
   end
 end
